db6fcc873fd336ede2b8dae15236d5dfd717fa5c
reftch <j.developer@hotmail.com>
2026-05-29 21:21:43 +0200

    Added schadcn, tailwindcss to the frontend part

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
   clearScreen: false,
 })
