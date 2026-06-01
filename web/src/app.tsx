9ddb89974719c58a1a47ed9da013de15d9f7a019
reftch <j.developer@hotmail.com>
2026-06-01 15:16:42 +0200

    Added skeleton for card

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "preact/hooks";
+import { BasicCard, SkeletonCard } from "./components/basic";
 import { Content } from "./components/content";
 import { Header, HeaderSearch, HeaderTitle } from "./components/header";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./components/ui/card";
-import type { City } from "./lib/model";
 import { Button } from "./components/ui/button";
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./components/ui/collapsible";
-import { ChevronDownIcon } from "lucide-react";
-import CityPanel from "./components/citypanel";
+import type { City } from "./lib/model";
 
 export function App() {
   const [city, setCity] = useState<City>();
@@ -20,8 +17,9 @@ export function App() {
 
   const onSearch = (city: City) => {
     if (city) {
-      console.log("Meteo for city:", city);
+      // console.log("Meteo for city:", city);
       sessionStorage.setItem("city-session", JSON.stringify(city));
+
 
       fetch(`/api/v1/temperature?latidude=${city.coordinate.latitude}&longtitude=${city.coordinate.longitude}`)
         .then((response) => response.json())
@@ -44,33 +42,7 @@ export function App() {
           </Header>
 
           <Content>
-            {city ? (
-              <Card className="w-full">
-                <CardHeader className="flex items-center gap-2 space-y-0 border-b  sm:flex-row">
-                  <div className="grid flex-1 gap-1 px-3">
-                    <CardTitle className="text-xl">{city.city}</CardTitle>
-                    <CardDescription>{city.state ? `${city.state}, ${city.country}` : city.country}</CardDescription>
-                  </div>
-                  <div className="px-3">{new Date().toLocaleString('de')}</div>
-                </CardHeader>
-                <CardContent className="items-center">
-                  <div className="px-3 text-2xl pb-3">{city?.current.temperature_2m}°</div>
-                  <Collapsible className="rounded-md data-[state=open]:bg-muted">
-                    <CollapsibleTrigger asChild>
-                      <Button variant="ghost" className="group">
-                        Place details
-                        <ChevronDownIcon className="ml-auto group-data-[state=open]:rotate-180" />
-                      </Button>
-                    </CollapsibleTrigger>
-                    <CollapsibleContent className="flex flex-col items-start gap-2 p-2.5 pt-0 text-sm">
-                      <div>
-                        <CityPanel city={city} />
-                      </div>
-                    </CollapsibleContent>
-                  </Collapsible>
-                </CardContent>
-              </Card>
-            ) : null}
+            {city ? <BasicCard city={city} /> : <SkeletonCard />}
           </Content>
 
         </div>
@@ -78,3 +50,4 @@ export function App() {
     </>
   )
 }
+
--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { City } from "./model"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function defaultCity(): City | (() => City) {
+  return {
+    id: 0,
+    city: 'Loading...',
+    state: '',
+    country: '',
+    elevation: 0,
+    coordinate: {
+      latitude: 0,
+      longitude: 0,
+    },
+    current: {
+      temperature_2m: 0,
+      interval: 0,
+      time: '',
+    },
+  }
 }
